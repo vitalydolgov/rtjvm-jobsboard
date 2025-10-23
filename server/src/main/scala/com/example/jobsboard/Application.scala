2db0d02b268e76fecb754871760e2e57899785a0
Vitaly Dolgov <vitaly.dolgov@posteo.net>
2025-10-23 13:41:05 +0200

    Warn if database is unreachable

--- server/src/main/scala/com/example/jobsboard/Application.scala
+++ server/src/main/scala/com/example/jobsboard/Application.scala
@@ -1,6 +1,7 @@
 package com.example.jobsboard
 
 import cats.effect.*
+import cats.implicits.*
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.*
 import org.http4s.HttpRoutes
@@ -11,6 +12,8 @@ import pureconfig.ConfigSource
 import pureconfig.error.ConfigReaderException
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
+import doobie.*
+import doobie.implicits.*
 
 import com.example.jobsboard.modules.*
 import com.example.jobsboard.config.*
@@ -19,10 +22,15 @@ import com.example.jobsboard.config.syntax.*
 object Application extends IOApp.Simple {
   given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
+  private def databaseHealth[F[_]: Async: Logger](xa: Transactor[F]): F[Boolean] = {
+    sql"SELECT 1".query[Int].unique.transact(xa).attempt.map(_.isRight)
+  }
+
   override def run: IO[Unit] = ConfigSource.default.loadF[IO, AppConfig].flatMap {
     case AppConfig(postgresConfig, emberConfig, securityConfig, tokenConfig, emailServiceConfig) =>
       val appResource = for {
         xa <- Database.makePostgresResource[IO](postgresConfig)
+        dbReady <- Resource.eval[IO, Boolean](databaseHealth(xa))
         core <- Core[IO](xa, tokenConfig, emailServiceConfig)
         httpApi <- HttpApi[IO](core, securityConfig)
         server <- EmberServerBuilder
@@ -31,8 +39,12 @@ object Application extends IOApp.Simple {
           .withPort(emberConfig.port)
           .withHttpApp(CORS(httpApi.endpoints).orNotFound)
           .build
-      } yield server
+      } yield (server, dbReady)
 
-      appResource.use(_ => IO.println("Server is ready!") *> IO.never)
+      appResource.use { (_, dbReady) =>
+        val warnIssues =
+          if (!dbReady) Logger[IO].warn(s"Database is not reachable!") else IO.unit
+        warnIssues *> IO.println("Server is ready!") *> IO.never
+      }
   }
 }
